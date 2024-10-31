11e1c9b30dab9e539075d0cd73b0c4d561fa0c11
Wahba Kamaluddin Nurdin <63429284+wahbakamaluddin@users.noreply.github.com>
2024-10-31 13:06:16 +0800

    First commit

---
+++ counter.js
@@ -0,0 +1,7 @@
+// document.addEventListener("click", () => {
+//     chrome.runtime.sendMessage("page-click");
+// });
+
+document.addEventListener("click", () => {
+  chrome.runtime.sendMessage("page-click");
+});
---
+++ service-worker.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener(async (msg) => {
+  if (msg === "page-click") {
+    const data = await chrome.storage.local.get({ counter: 0 });
+    let counter = data["counter"];
+    counter++;
+    chrome.storage.local.set({ counter });
+
+    await chrome.action.setBadgeText({ text: counter.toString() });
+  }
+});
